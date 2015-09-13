0d1c42263a3b5d98ca5971809c2d96f9044af4e1
Luis Roberto Flores <sys99+git@mm.st>
2015-09-13 10:05:27 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+	var numeroUsuario;
+
+	var numeroRandom = Math.floor((Math.random() * 100) + 1);
+
+	var win = false;
+
+	//$('#random').text(numeroRandom);
+
+	// FUNCION MENSAJE
+	var enviarMensaje = function (mensaje) {
+		$('#mensaje').text(mensaje);
+	}
+	var enviarAlerta = function (mensaje) {
+		$('#alerta').text(mensaje);
+	}
+
+	// FUNCION VERIFICAR NÚMERO
+	var verificarNumero = function (n){
+		if(isNaN(n)) {
+			enviarAlerta('Escribe solo números por favor');
+			return true;
+		} else if (n < 1 || n > 100) {
+			enviarAlerta('El número debe estar entre 1 y 100');
+			return true;
+		} else {
+			enviarAlerta('El número es: '+ n);
+			return false;
+		}
+	};
+
+	//FUNCION NÚMERO A LISTA
+	var listaNumeros = function(i) {
+		$('#lista').append('<li>' + i + '</li>');
+	};
+
+	//FUNCION COMPARAR NÚMERO
+	var compararNumero = function (n,r){
+		var restaNumeros = r - n;
+		var resultado = Math.abs(restaNumeros);
+		if (n === r) {
+			enviarMensaje('Has ganado');
+			win = true;
+		} else if (resultado > 20) {
+			enviarMensaje('Muy Frio');
+		} else if (resultado > 10 && resultado <= 20) {
+			enviarMensaje('Frio');
+		} else if (resultado <= 10 && resultado >= 5) {
+			enviarMensaje('Caliente');
+		} else if (resultado >= 1 && resultado < 5) {
+			enviarMensaje('Muy Caliente');
+		}
+	};
+
+	$('form').submit(function(event){
+		
+		event.preventDefault(); // para evitar que se recargue al página al enviar.
+		numeroUsuario = parseInt($('#inNumero').val());
+		if (verificarNumero(numeroUsuario) ===  false){
+			listaNumeros(numeroUsuario);
+			compararNumero(numeroUsuario, numeroRandom);
+		} else {
+			enviarMensaje('Intenta de nuevo');
+		};
+		return numeroUsuario;
+
+	});
+
+});
